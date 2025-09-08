f9b42692546761c4a2fcc054fc6e12756578de27
sonukumawat12 <sonukuaa@gmail.com>
2025-09-08 09:02:07 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/new-portfolio/',   // 👈 yeh line add karo
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
