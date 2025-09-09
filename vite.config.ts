82ac711aef1fcf9eeb8f0365834ebc84e3106c57
sonukumawat12 <sonukuaa@gmail.com>
2025-09-09 09:40:38 +0530

    sasas

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio2/',
+  base: '/sonu-webdev-portfolio/',
   plugins: [
     react({
-      // Optimize JSX runtime
+      // Optimize JSX runtime11111111111
       jsxRuntime: 'automatic',
     }),
   ],
