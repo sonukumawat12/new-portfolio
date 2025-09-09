4f3ca47bec4c55afed3b9030d49ae0c4c0c73ea3
sonukumawat12 <157627837+sonukumawat12@users.noreply.github.com>
2025-09-09 09:01:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio2/',
+  base: '/sonu-webdev-portfolio/',
   plugins: [
     react({
       // Optimize JSX runtime
